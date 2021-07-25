4427dfc528dae7bccbc813cf38c2aaa979e8b956
Jochen Rui <36842990+jochenrui@users.noreply.github.com>
2021-07-25 17:58:18 +0200

    added employee roles

---
+++ employees/roles.py
@@ -0,0 +1,11 @@
+from enum import Enum, auto
+
+
+class Roles(Enum):
+    """
+    Enum for Employee Roles
+    """
+    CTO = "cto"
+    DEV = "dev"
+    JUNIORDEV = "juniordev"
+    INTERN = "intern"
